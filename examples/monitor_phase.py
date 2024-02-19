3990531a1b5b469816268589423c5e77312d638d
Tangui Aladjidi <taladjidi@gmail.com>
2024-02-19 20:34:01 +0100

    monitor_phase with pre planning
    x3 speed improvement

--- examples/monitor_phase.py
+++ examples/monitor_phase.py
@@ -7,6 +7,7 @@ from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 import pickle
 import contrast
 import matplotlib.style as mplstyle
+from typing import Any
 
 mplstyle.use('fast')
 plt.switch_backend('Qt5Agg')
@@ -26,7 +27,7 @@ except FileNotFoundError:
 # between reference and signal beams as described in the demo_contrast 
 # notebook
     
-def monitor_fourier_fringes(cam: any) -> None:
+def monitor_fourier_fringes(cam: Any) -> None:
     """Displays a pop up window to monitor in live the position of satellite peaks
     in the Fourier space
 
@@ -90,50 +91,52 @@ def monitor_fourier_fringes(cam: any) -> None:
                                     interval=50, blit=True)
     plt.show(block=True)
 
-def monitor_phase(cam: any) -> None:
-    """Displays a pop up window to monitor in live the phase of the field.
-
-    Args:
-        cam (any): the camera object
-
-    Returns:
-        None
-    """
-    # grab first frame for reference
-    ret, frame = cam.read()
-    im_fringe = contrast.im_osc_fast_t(frame, cont=False)
-    phase = contrast.angle_fast(im_fringe)
-    cont = np.abs(im_fringe)
-    fig, ax = plt.subplots(1, 2)
-    divider0 = make_axes_locatable(ax[0])
-    cax0 = divider0.append_axes("right", size="5%", pad=0.05)
-    divider1 = make_axes_locatable(ax[1])
-    cax1 = divider1.append_axes("right", size="5%", pad=0.05)
-    im0 = ax[0].imshow(phase, cmap='twilight_shifted',
-                        vmin=-np.pi, vmax=np.pi)
-    im1 = ax[1].imshow(cont**2/np.max(cont**2),
-                        cmap='viridis', vmin=0, vmax=1)
-    fig.colorbar(im0, cax=cax0)
-    fig.colorbar(im1, cax=cax1)
-    ax[0].set_title("Phase")
-    ax[1].set_title("Intensity")
-
-    def animate(i):
-        """Animation function for the window
-
-        Args:
-            i (int): Counter from the animate function
+def monitor_phase(cam: Any):
+        """Displays a pop up window to monitor in live the phase
         """
-        ret, frame[:, :] = cam.read()
-        im_fringe[:, :] = contrast.im_osc_fast_t(frame, cont=False)
-        phase[:, :] = contrast.angle_fast(im_fringe)
-        cont[:, :] = np.abs(im_fringe)
-        im0.set_data(phase)
-        im1.set_data(cont**2/np.max(cont**2))
-        return im0, im1
-    anim = animation.FuncAnimation(fig, animate,
-                                    interval=33, blit=True)
-    plt.show(block=True)
+        frame = cam.capture().astype(np.float32)
+        plan_rfft = pyfftw.builders.rfft2(frame)
+        plan_ifft = pyfftw.builders.ifft2(
+            frame[0:frame.shape[0]//2, 0:frame.shape[1]//2])
+        plans = (plan_rfft, plan_ifft)
+        im_fringe = contrast.im_osc_fast_t(frame, cont=False, plans=plans)
+        phase = contrast.angle_fast(im_fringe)
+        cont = im_fringe.real*im_fringe.real + im_fringe.imag*im_fringe.imag
+        fig, ax = plt.subplots(1, 2)
+        divider0 = make_axes_locatable(ax[0])
+        cax0 = divider0.append_axes("right", size="5%", pad=0.05)
+        divider1 = make_axes_locatable(ax[1])
+        cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+        im0 = ax[0].imshow(phase, cmap='twilight_shifted',
+                           vmin=-np.pi, vmax=np.pi)
+        im1 = ax[1].imshow(cont,
+                           cmap='viridis', vmin=0, vmax=1)
+        fig.colorbar(im0, cax=cax0)
+        fig.colorbar(im1, cax=cax1)
+        ax[0].set_title("Phase")
+        ax[1].set_title("amplitude")
+
+        def animate(i):
+            """Animation function for the window
+
+            Args:
+                i (int): Counter from the animate function
+            """
+            frame = self.cam_real.capture_video_frame(buffer_=frame_buf)
+            im_fringe[:, :] = contrast.im_osc_fast_t(
+                frame, cont=False, plans=plans)
+            phase[:, :] = contrast.angle_fast(im_fringe)
+            cont[:, :] = im_fringe.real*im_fringe.real + im_fringe.imag*im_fringe.imag
+            im0.set_data(phase)
+            im1.set_data(cont)
+            im1.set_clim(np.nanmin(cont), np.nanmax(cont))
+            return im0, im1
+        anim = animation.FuncAnimation(fig, animate,
+                                       interval=0, blit=True,
+                                       cache_frame_data=False)
+        plt.show(block=True)
+        self.cam_real.stop_video_capture()
+        self.toggle_ref()
 
 def monitor_fourier_space(cam: any) -> None:
     """Displays a pop up window to monitor in live the Fourier space
