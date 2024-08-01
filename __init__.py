eb11e30aae99adcbf015fac2e6c8fb1398154c95
Tangui Aladjidi <taladjidi@gmail.com>
2024-08-01 10:39:49 +0200

    Licensing

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ A package containing all the relevant utilities to process phase data of optical
 
 __version__ = "1.0.0"
 __author__ = "Tangui Aladjidi"
-__license__ = "MIT"
+__license__ = "GPLv3"
 __credits__ = "Laboratoire Kastler Brossel, Paris, France"
 __email__ = "tangui.aladjidi@lkb.upmc.fr"
